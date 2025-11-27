3bbcfd4fdc7cca27a1b81fedefb410e6f7f262f2
bentevissel <univ5946@ox.ac.uk>
2025-11-27 16:09:58 +0000

    created classes infectionforce and infectioncount, added tests (not finished)

---
+++ vaccbopti/classes/infectioncount.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+class InfectionCount:
+    '''
+    This class holds a dataframe with the number of people
+    that are asymptomatic and symptomatic infected
+    '''
+    #create a dictionary which holds the statuses and the corresponding age groups (index of 16)
+
+    def __init__(self):
+        df_status = ['symptomatic','asymptomatic']
+        self.count_df = pd.DataFrame(0, index=np.arange(16),columns=df_status)
+
+
+test_table = InfectionCount()
+print(test_table.count_df)
