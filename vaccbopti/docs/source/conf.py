c7b0cceef0dad7785a13c2d0c1e3d1c08ae87c62
An Mei Daniels <amybda2004@outlook.com>
2025-11-27 10:12:56 +0000

    Added readthedocs and sphinx

---
+++ vaccbopti/docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Vaccine Booster Optimisation'
+copyright = '2025, An Mei Daniels, Bente Vissel, Monica Dewi, Kristijonas Raibuzis & Abbie Evans'
+author = 'An Mei Daniels, Bente Vissel, Monica Dewi, Kristijonas Raibuzis & Abbie Evans'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
