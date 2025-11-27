7d74687836884c06ffe96e54a1fa2bd1df57779a
An Mei Daniels <amybda2004@outlook.com>
2025-11-27 15:56:31 +0000

    Edited person.py to work with singleton class + flake8 edits

--- vaccbopti/classes/person.py
+++ vaccbopti/classes/person.py
@@ -1,4 +1,4 @@
-### FILE CONTAINING ALL THE RELEVANT INFORMATION AND FUNCTIONS FOR A PERSON IN THE SIMULATION.
+# FILE CONTAINING ALL THE RELEVANT INFORMATION AND FUNCTIONS FOR A PERSON IN THE SIMULATION.
 
 
 # Import useful modules
@@ -20,7 +20,7 @@ class Person:
             prob_infected (float): probability an individual will become infected
             latent_t_i (int): time left in the latent period once exposed
             infect_t_i (int): time left in infectious period once infectious (symptomatic or asymptomatic)
-            hosp_t_i (int): time spent in hospital 
+            hosp_t_i (int): time spent in hospital
             death_t_i (int): time at death from hospitalisation
             immunity_time_exvacc (int): time since given last vaccination/sickness from previous strain
             immunity_time_newvacc (int): time since given since strain-adapted vaccine
@@ -35,80 +35,79 @@ class Person:
         self.death_t_i = -1
         self.immunity_time_exvacc = -1
         self.immunity_time_newvacc = -1
-        self.immunity_time_infec = -1 
+        self.immunity_time_infec = -1
 
     def get_age_group(self, n):
         """Assign individual a specific age-group.
         Params:
             n (int): index for a specific age group from array of age groups
         """
-        self.age_group = str(Params.age_groups[n])
-    
+        self.age_group = str(Params.instance.age_groups[n])
+
     def calc_prob_exposed(self):
         """Calculates the probability a person's status changes from susceptible to exposed."""
         exp_val = np.exp(- self.calc_susceptibility() * infection_force.InfectionForce)
         self.prob_exposed = 1 - exp_val
-
 
     def calc_susceptibility(self):
         """Calculates the relative susceptibility, v(t), of an individual.
         Returns:
             susceptibility (float): level of susceptibility is determined by their immune status
         """
-        if self.immunity_time_exvacc < 0 : 
-            immunity_exvacc = 0 
+        if self.immunity_time_exvacc < 0:
+            immunity_exvacc = 0
         else:
-            immunity_exvacc = Params.f_exvacc[self.immunity_time_exvacc]
+            immunity_exvacc = Params.instance.f_exvacc[self.immunity_time_exvacc]
         if self.immunity_time_newvacc < 0:
             immunity_newvacc = 0
         else:
-            immunity_newvacc = Params.f_newvacc[self.immunity_time_newvacc]
+            immunity_newvacc = Params.instance.f_newvacc[self.immunity_time_newvacc]
         if self.immunity_time_infec < 0:
             immunity_infec = 0
         else:
-            immunity_infec = Params.f_infec[self.immunity_time_infec]
+            immunity_infec = Params.instance.f_infec[self.immunity_time_infec]
         susceptibility = 1 - max(immunity_exvacc,
                                  immunity_newvacc,
-                                 immunity_infec)      
+                                 immunity_infec) 
         return susceptibility
-    
+
     def change_status(self):
         """Decision tree to determine a person's status at each time step."""
         # If infected in any condition, then count down until recovered and back to susceptible population
-        if self.status == 'symptomatic' or self.status == 'asymptomatic' or self.status=='hospitalised':
+        if self.status == 'symptomatic' or self.status == 'asymptomatic' or self.status == 'hospitalised':
             self.infect_t_i -= 1
             if self.infect_t_i == -1:
                 self.status = 'susceptible'
         # If exposed, count down until latent period is finished and then determine response to infection
         if self.status == 'exposed':
-            self.latent_t_i -= 1 
-            if self.latent_t_i == -1: #if latent period is finished, determine type of infection
-                self.determine_status_change(["symptomatic", "asymptomatic"], Params.p_v_symp_a) #determined symptomatic or asymptomatic
-                self.infect_t_i = self.pick_distr_prob(self, Params.infectious_time) #determine infectious period time
-                if self.status == 'symptomatic': #if symptomatic
-                    self.determine_status_change(['symptomatic','hospitalised'], Params.p_nv_IH) #check if they become hospitalised
-                    if self.status == 'hospitalised': #if hospitalised, calculate how long in hospital
-                        self.hosp_t_i = self.pick_distr_prob(Params.hospitalistion_time)
-                        self.determine_status_change(['hospitalised','dead'], Params.p_nv_HD) #check if they die
-                        if self.status == 'dead': #if they die, calculate how long it takes
-                            self.death_t_i = self.hosp_t_i + self.pick_distr_prob(Params.death_time)
+            self.latent_t_i -= 1
+            if self.latent_t_i == -1:  # if latent period is finished, determine type of infection
+                self.determine_status_change(["symptomatic", "asymptomatic"], Params.instance.p_v_symp_a)  # (a)symptomatic or not
+                self.infect_t_i = self.pick_distr_prob(self, Params.instance.infec_period))  # determine infectious period time                   
+                if self.status == 'symptomatic':  # if symptomatic
+                    self.determine_status_change(['symptomatic', 'hospitalised'], Params.instance.p_nv_IH)  # check if hospitalised
+                    if self.status == 'hospitalised':  # if hospitalised, calculate how long in hospital
+                        self.hosp_t_i = self.pick_distr_prob(Params.instance.hosp_time)
+                        self.determine_status_change(['hospitalised', 'dead'], Params.instance.p_nv_HD)  # check if they die
+                        if self.status == 'dead':  # if they die, calculate how long it takes
+                            self.death_t_i = self.hosp_t_i + self.pick_distr_prob(Params.instance.death_period)
         # If a person is susceptible, see if they become exposed
         if self.status == 'susceptible':
-            self.determine_status_change(['susceptible','exposed'], self.prob_exposed)
-            if self.status == 'exposed': #once exposed, choose time until infected
-                self.latent_t_i = self.pick_distr_prob(Params.latent_time)
+            self.determine_status_change(['susceptible', 'exposed'], self.prob_exposed)
+            if self.status == 'exposed':  # once exposed, choose time until infected
+                self.latent_t_i = self.pick_distr_prob(Params.instance.latent_time)
 
     def determine_status_change(self, statuses, probability):
         """Determines if the person's status, based on probability.
-        Params: 
+        Params:
             statuses (list): a list of the two possible statuses
             probability (float or list): a float of probability or list of probabilities per age group
         """
         if type(probability) is list:
-            index = np.where(Params.age_groups == self.age_group)[0][0]
-            status = np.random.choice(statuses, size=1, p=[probability[index], 1-probability[index]])
+            index = np.where(Params.instance.age_groups == self.age_group)[0][0]
+            status = np.random.choice(statuses, size=1, p=[probability[index], 1 - probability[index]])
         else:
-            status = np.random.choice(statuses, size=1, p=[probability, 1-probability])
+            status = np.random.choice(statuses, size=1, p=[probability, 1 - probability])
         self.status = str(status[0])
 
     def pick_distr_prob(self, distribution):
