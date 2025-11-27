0bf9aff68abac1961a242f07aaae5b005af22f16
An Mei Daniels <amybda2004@outlook.com>
2025-11-27 10:53:28 +0000

    Started adding info for classes

--- vaccbopti/classes/params.py
+++ vaccbopti/classes/params.py
@@ -1,1 +1,18 @@
 # File for parameters
+
+class params:
+    """Class containing all the general information."""
+    
+    age_groups = ['0-4', '5-9', '10-14', '15-19', 
+                  '20-24', '25-29', '30-34', '35-39', 
+                  '40-44', '45-49', '50-54', '55-59', 
+                  '60-64', '65-69', '70-74', '75+']
+    n_indivs_a = [5758, 6112, 5849, 5413, 
+                  6011, 6698, 6828, 6691, 
+                  6424, 6311, 6889, 6696, 
+                  5769, 5015, 5021, 8515]
+    
+    @classmethod
+    def access_age_groups(cls):
+        """Method to access the age groups in this class, to then assign one of the age groups to a person."""
+        return cls.age_groups
--- vaccbopti/classes/person.py
+++ vaccbopti/classes/person.py
@@ -1,1 +1,14 @@
 # File for person class
+
+class Person:
+    def __init__(self):
+        self.age_group = None
+        self.status = 'susceptible' #susceptible, exposed, infectious symptomatic, infectious asymptomatic, hospitalised, dead
+    def get_age_group(self, n):
+        self.age_group = params.access_age_groups()[n]
+    def calc_susceptibility():
+        susceptibility = 1 - max(calc_fx(), calc_fx(), calc_fx())
+        return susceptibility
+    def calc_fx()
+        
+        return f_x
