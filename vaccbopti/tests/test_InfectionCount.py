c91a4422dbce20dea17bd56cd4341a42fe875728
KristjonasR <scro5102@ox.ac.uk>
2025-11-27 17:14:37 +0000

    fixes

--- vaccbopti/tests/test_InfectionCount.py
+++ vaccbopti/tests/test_InfectionCount.py
@@ -9,13 +9,13 @@ from vaccbopti.classes.params import Params
 
 class TestInfectionCount(TestCase):
     def setUp(self):
-        self.testTable = InfectionCount().count_df
+        self.testTable = InfectionCount.instance().count_df
         self.ageNbs = Params.instance().age_groups
 
     def test_df_len(self):
         len_df = len(self.testTable)
         len_age_groups = len(self.ageNbs)
-        self.assert_equal(len_df, len_age_groups)
+        self.assertEqual(len_df, len_age_groups)
 
 if __name__ == '__main__':
     unittest.main()
