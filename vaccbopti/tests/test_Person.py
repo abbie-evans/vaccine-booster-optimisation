6f5c8ace29b75cd608aacd3923f0aa35f6519bbb
An Mei Daniels <amybda2004@outlook.com>
2025-11-28 12:58:29 +0000

    Added person unittests + flake8

--- vaccbopti/tests/test_Person.py
+++ vaccbopti/tests/test_Person.py
@@ -1,10 +1,13 @@
 # FILE FOR TESTING THE PERSON CLASS
 
 # Import useful modules
-from vaccbopti.classes.params import Params
-from vaccbopti.classes.person import Person
+import sys
+sys.path.insert(0, "C:/Users/lina4801/OneDrive - Nexus365/Team-Project-Sandpit/vaccine-booster-optimisation")
+import numpy as np
 import unittest
 from unittest import TestCase
+from vaccbopti.classes.person import Person
+from vaccbopti.classes.params import Params
 params = Params.instance()
 
 
@@ -17,9 +20,25 @@ class test_person(TestCase):
         self.testPerson = Person()
 
     def test_get_age_group(self):
+        """Test that the ages are called correctly and can be reindexed correctly."""
         self.assertEqual(self.testPerson.age_group, None)
-        self.testPerson.get_age_group(0)
-        self.assertEqual(self.testPerson.age_group, '0-4')
+        self.testPerson.get_age_group(4)
+        self.assertEqual(self.testPerson.age_group, "20-24")
+        index = np.where(params.age_groups == self.testPerson.age_group)[0][0]
+        self.assertEqual(index, 4)
+    
+    def test_calc_susceptibility(self):
+        """Test that susceptibility calculations are correct."""
+        self.assertEqual(self.testPerson.calc_susceptibility(), 1)
+        self.testPerson.immunity_time_exvacc = 5
+        self.assertAlmostEqual(self.testPerson.calc_susceptibility(), 1 - 0.22898922)
+    
+    def test_determine_status_change(self):
+        self.testPerson.get_age_group(4)
+        self.testPerson.determine_status_change(['symptomatic', 'asymptomatic'], 1)
+        probs = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+        self.testPerson.determine_status_change(["symptomatic", "asymptomatic"], probs)
+        self.assertEqual(self.testPerson.status, 'asymptomatic')
 
 
 if __name__ == "__main__":
