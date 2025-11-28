e4a72221dea7e77cb0824a287febaf20c8cad098
bentevissel <univ5946@ox.ac.uk>
2025-11-28 11:44:07 +0000

    changed the classes files to correctly access the modules; updated init files, WIP test_InfectionForce

--- vaccbopti/__init__.py
+++ vaccbopti/__init__.py
@@ -1,4 +1,4 @@
 # here note down all the functions that are included in the package
-
+from vaccbopti import classes
 # and include them in the file
 __all__ = [""]
--- vaccbopti/classes/__init__.py
+++ vaccbopti/classes/__init__.py
@@ -1,4 +1,8 @@
 # here note down all the functions that are included in the package
+from .infectioncount import InfectionCount
+from .infectionforce import InfectionForce
+from .params import Params
+from .person import Person
 
-# and include them in the file
-__all__ = [""]
+# and include them in the fi"le
+__all__ = ["InfectionCount", "InfectionForce", "Params", "Person"]
