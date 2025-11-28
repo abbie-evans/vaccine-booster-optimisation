f7408c97809373fe66dc0a94dc94404d9c82a08b
Lay Monica Ratna Dewi <125332442+laymonicard@users.noreply.github.com>
2025-11-28 12:21:43 +0000

    Upload test file for params.py

---
+++ vaccbopti/tests/test_Params.py
@@ -0,0 +1,20 @@
+# test file for ensuring error raised are as expected
+
+import unittest
+from unittest import TestCase
+from vaccbopti.classes.params import Params
+
+# check if the RuntimeError is raised when the Params._instance contains an object
+class TestParams(TestCase):
+
+    def test_error_more_than_one_instance(self):
+        with self.assertRaises(RuntimeError) as ve:
+            params = Params() #equivalent to __init__(self)
+        self.assertEqual("This class is a singleton!", str(ve.exception))
+
+    def test_error_instance_not_created(self):
+        test_var = Params.instance().infec_asymp
+        self.assertEqual(0.255, test_var)
+
+if __name__ == '__main__':
+    unittest.main()
