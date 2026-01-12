2371605909c5692dd2ff3c3ef7bf5d7b6816badc
bentevissel <univ5946@ox.ac.uk>
2026-01-12 11:58:58 +0000

    fixed flake8 cries

--- vaccbopti/tests/test_InfectionForce.py
+++ vaccbopti/tests/test_InfectionForce.py
@@ -50,12 +50,8 @@ class TestInfectionForce(TestCase):
         #the function should run over 16 age groups
         loop_times = 16
         self.test_nb =  float(1.1)
-
         self.assertEqual(loop_times, test_nb)
         self.assertEqual(type(test_function), type(self.test_nb))
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
