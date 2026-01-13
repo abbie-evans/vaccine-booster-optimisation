64294789ae9f48ce1994e3e03ad589e029c917e9
An Mei Daniels <amybda2004@outlook.com>
2026-01-13 12:37:14 +0000

    update tests to improve commenting and coverage

--- vaccbopti/tests/test_Params.py
+++ vaccbopti/tests/test_Params.py
@@ -1,20 +1,23 @@
-# Test file for ensuring errors raised are as expected
+# FILE FOR TESTING THE PARAMS CLASS SO THAT ERRORS RISE WHERE THEY SHOULD
 
-
+# Import useful modules
+import unittest
 from unittest import TestCase
 from vaccbopti.classes.params import Params
 
-# Check if the RuntimeError is raised when the Params._instance contains an object
 
-
+# Define testing class
 class TestParams(TestCase):
+    """A class to test that the Person class is set up and runs correctly."""
 
     def test_error_more_than_one_instance(self):
+        """Check if RuntimeError is raised when Params contains an object."""
         with self.assertRaises(RuntimeError) as ve:
-            Params()  # Equivalent to __init__(self)
+            Params()  # equivalent to __init__(self)
         self.assertEqual("This class is a singleton!", str(ve.exception))
 
     def test_error_instance_not_created(self):
+        """Check to make sure that it exists when an instance is set up."""
         test_var = Params.instance().infec_asymp
         self.assertEqual(0.255, test_var)
 
