3b8501bd1659a56f40c3133155cd86c93029d1a9
An Mei Daniels <amybda2004@outlook.com>
2026-06-30 13:49:45 +0100

    initial set up for the user interface

--- vaccbopti/app.py
+++ vaccbopti/app.py
@@ -1,7 +1,86 @@
-from shiny.express import ui
+# Import useful modules
+import os
+import itertools
+import numpy as np
+import pandas as pd
+from shiny import reactive
+from shiny.express import input, render, ui
+from vaccbopti.classes import Params
+from vaccbopti.classes import InfectionCount
+from vaccbopti.classes import InfectionForce
+from vaccbopti.classes import Timesteps
+params = Params.instance()
+infectioncount = InfectionCount().count_df
+infection_force = InfectionForce()
+project_root = os.path.dirname(os.path.dirname(__file__))
 
-with ui.sidebar(bg="#f8f8f8"):  
-    "Sidebar" 
+# Title
+ui.page_opts(title="Vaccine Booster Optimisation Simulations", fillable=True)
 
+# Sidebar to hold all the user inputs
+with ui.sidebar(position="left"):
 
-"Main content"
+    # Dark Mode
+    ui.input_dark_mode(mode="light")
+
+    # Allow you to either run a simulation or load previous data
+    with ui.accordion(id='inputs', open='Run Simulation'):
+        # Simulation inputs
+        with ui.accordion_panel('Run Simulation'):
+            # Number of runs
+            with ui.tooltip(id="number_runs_tooltip", placement="right"):
+                ui.input_numeric("number_runs", "Number of Runs", 10, min=1, step=1)
+                "The number of times you want to run a simulation."
+            # Simulation Length
+            with ui.tooltip(id="sim_length_tooltip", placement="right"):
+                ui.input_numeric("sim_length", "Simulation Timesteps", 100, min=5, step=1)
+                "The number of timesteps (days) each simulation will run for."
+            # Number of people
+            with ui.tooltip(id="num_people_tooltip", placement="right"):
+                ui.input_numeric("num_people", "Number of People", 100, min=10, step=1)
+                "The number of people to have in the simulation."
+            # Number of Infected
+            with ui.tooltip(id="n_infec_tooltip", placement="right"):
+                ui.input_numeric("n_infec", "Initially Infected People", 40, min=0, step=1)
+                "The number of people who start the simulation exposed to the new variant."
+            @reactive.effect
+            def _():
+                num_people = input.num_people()
+                n_infec = input.n_infec()
+                if n_infec > num_people:
+                    n_infec = num_people
+                ui.update_numeric("n_infec", value=n_infec, max=num_people)
+            # R_e
+            with ui.tooltip(id="R_e_tooltip", placement="right"):
+                ui.input_numeric("R_e", "Transmissability of New Variant", 1.5, min=0.1, step=0.01)
+                "The transmissability, R_e, of the novel varient."
+            # Vaccine Strategy
+            with ui.tooltip(id="vacc_strat_tooltip", placement="right"):
+                ui.input_selectize("vacc_strat", "Vaccine Administration Strategy",
+                                {0: "No Booster",
+                                1: "Strategy 1",
+                                2: "Strategy 2",
+                                3: "Strategy 3",
+                                4: "Strategy 4",
+                                5: "Strategy 5",
+                                6: "Strategy 6"})
+                "Which booster vaccine administration strategy to use - see the introduction for more details."
+            # Number of Vaccines to Give Out
+            with ui.tooltip(id="vacc_amount_tooltip", placement="right"):
+                ui.input_numeric("vacc_amount", "Number of Boosters Admistered Per Day", 10, min=1)
+                "The number of booster vaccines to administer per day."
+            # New Vaccine Availability
+            with ui.tooltip(id="t_newacc_avail_tooltip", placement="right"):
+                ui.input_numeric("t_newacc_avail", "New Vaccine Availability", 10, min=1)
+                "Which day the new booster vaccine (for the new variant) is available to be administered. Only a vaccine for an old variant (which is less effective) is available before this."
+
+        # Load other csvs
+        with ui.accordion_panel('Load inputs'):
+            'Load csv'
+
+with ui.navset_card_pill(id="tab"):
+    with ui.nav_panel("Introduction"):
+        "Panel A content for testing"
+    with ui.nav_panel("Outputs"):
+        "Panel B content"
+
