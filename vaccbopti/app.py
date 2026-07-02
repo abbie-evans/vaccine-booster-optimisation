b72f1728af5243a859e6afa848d0817fedae5a22
An Mei Daniels <amybda2004@outlook.com>
2026-07-02 15:07:10 +0100

    started to update combined std

--- vaccbopti/app.py
+++ vaccbopti/app.py
@@ -319,9 +319,8 @@ def parsed_file():
 @reactive.calc
 def agg_data():
     label = input.strategy()
-    sum_path = f'{strategy_dir}/model_example_strategy_{label}_mean.csv'
     sd_path = f'{strategy_dir}/model_example_strategy_{label}_std_combined.csv'
-    df_sum, df_sd = load_status_data(sum_path, sd_path)
+    df_sum, df_sd = load_data(label)
     df_agg = aggregate_status(df_sum, group_cols=['t', 'vacc_status'])
     df_sd_agg = load_combined_sd(sd_path)
     return df_agg, df_sd_agg
