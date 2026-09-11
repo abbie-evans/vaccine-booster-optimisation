69cfee83ba1f49a964220e02b68188c2ddd20857
Abbie Evans <147088150+abbie-evans@users.noreply.github.com>
2026-09-11 16:40:09 +0100

    Update readthedocs

--- vaccbopti/docs/source/conf.py
+++ vaccbopti/docs/source/conf.py
@@ -6,15 +6,42 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+import sphinx
+
+# Make the project root importable
+sys.path.insert(0, os.path.abspath('../..'))
+
 project = 'Vaccine Booster Optimisation'
-copyright = '2025, An Mei Daniels, Bente Vissel, Monica Dewi, Kristijonas Raibuzis & Abbie Evans'
+copyright = '2026, Abbie Evans, An Mei Daniels, Bente Vissel, Monica Dewi & Kristijonas Raibuzis'
 author = 'Abbie Evans, An Mei Daniels, Bente Vissel, Monica Dewi & Kristijonas Raibuzis'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+# Autodoc defaults
+if int(sphinx.__version__.split('.')[1]) < 8:
+    autodoc_default_flags = [
+        'members',
+        'inherited-members',
+    ]
+else:
+    autodoc_default_options = {
+        'members': None,
+        'inherited-members': None,
+    }
+
+master_doc = 'index'
 
 templates_path = ['_templates']
 exclude_patterns = []
