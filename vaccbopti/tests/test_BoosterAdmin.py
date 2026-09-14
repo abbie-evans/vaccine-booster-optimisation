ebe5258ed150a30229d1fbd85914723b94e1e565
An Mei Daniels <amybda2004@outlook.com>
2026-09-14 13:11:20 +0100

    flake8 please i take back what i said before just set me free

--- vaccbopti/tests/test_BoosterAdmin.py
+++ vaccbopti/tests/test_BoosterAdmin.py
@@ -58,24 +58,24 @@ class testBoosterAdmin(TestCase):
         """Test that vaccine administration, happens descending in order."""
         # Run and check to make sure the correct people (old people descending order)
         self.admin.vaccine_administration(self.people, 10, vaccine_choice='ex_vacc',
-                                            direction='descend', age_targets='mid-old')
+                                          direction='descend', age_targets='mid-old')
         remaining_eligible_unvacc = [p for p in self.people
-                                        if p.age_group in ['70-74', '75+']
-                                        and p.vacc_status == 'unvacc']
+                                     if p.age_group in ['70-74', '75+']
+                                     and p.vacc_status == 'unvacc']
         eligible_vacc = [p for p in self.people
-                            if p.age_group in ['70-74', '75+']
-                            and p.vacc_status == 'ex_vacc']
+                         if p.age_group in ['70-74', '75+']
+                         and p.vacc_status == 'ex_vacc']
         self.assertEqual(len(remaining_eligible_unvacc), 0)
         self.assertEqual(len(eligible_vacc), 10)
         # Run and check to make sure the correct people (young people descending order)
         self.admin.vaccine_administration(self.people, 10, vaccine_choice='new_vacc',
-                                            direction='descend', age_targets='mid-young')
+                                          direction='descend', age_targets='mid-young')
         remaining_eligible_unvacc = [p for p in self.people
-                                        if p.age_group in ['40-44', '45-49']
-                                        and p.vacc_status == 'unvacc']
+                                     if p.age_group in ['40-44', '45-49']
+                                     and p.vacc_status == 'unvacc']
         eligible_vacc = [p for p in self.people
-                            if p.age_group in ['40-44', '45-49']
-                            and p.vacc_status == 'new_vacc']
+                         if p.age_group in ['40-44', '45-49']
+                         and p.vacc_status == 'new_vacc']
         self.assertEqual(len(remaining_eligible_unvacc), 0)
         self.assertEqual(len(eligible_vacc), 10)
 
