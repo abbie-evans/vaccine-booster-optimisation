106dbd61a05d400dd83b3947140b68319f48c197
Abbie Evans <abbieevans75@gmail.com>
2026-09-16 13:55:15 +0100

    Include csv files

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(name='vaccbopti',
       url='https://github.com/abbie-evans/vaccine-booster-optimisation',
       # Packages to include
       packages=find_packages(include=('vaccbopti', 'vaccbopti.*')),
-      package_data={
-        'vaccbopti.classes': ['*.csv',]},
+      package_data={'vaccbopti.classes': ['*.csv',]},
       install_requires=['numpy',
                         'pandas',
                         'matplotlib',
